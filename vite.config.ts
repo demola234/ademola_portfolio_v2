96639f4b71a6f48bd33089b50470d7015992212b
demola234 <kolawoleoluwasegun567@gmail.com>
2024-11-24 11:27:04 +0100

    fix: vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-
+// https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.md"],
+  plugins: [react()],
+  base: "/",
   optimizeDeps: {
     include: ["react-markdown"],
   },
-  base: "/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["**/*.md"],
-      manifest: {
-        name: "Ademola Kolawole Portfolio",
-        short_name: "Ademola Kolawole",
-        description: "A simple portfolio to describe my experience",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
 });
