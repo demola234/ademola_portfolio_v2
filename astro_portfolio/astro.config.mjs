eeec31f5eff64693419a8418a2d00d722d7cefdc
Ademola Kolawole <kolawoleoluwasegun567@gmail.com>
2026-07-03 14:05:26 +0100

    feat: add Vercel adapter and update dependencies for static output

--- astro_portfolio/astro.config.mjs
+++ astro_portfolio/astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://demola.dev',
+  output: 'static',
+  adapter: vercel(),
   integrations: [react(), tailwind()],
 });
